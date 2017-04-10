07805259fcc7f9216603207b602d6503c5d574a3
Sam Corin <samcorin@gmail.com>
2017-04-10 18:56:09 +0900

    minor changes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,11 +1,35 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only: [ :show, :destroy ]
+
   def index
     @services = Service.all
   end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
-  def set_services
+  def set_service
     @service = Service.find(params[:id])
   end
+
+  # def dose_params
+  #   params.require(:service).permit()
+  # end
 end
