3ca2fd54fb24f0385445168a1d39a30c00dcf2d1
Sam Corin <samcorin@gmail.com>
2017-04-10 19:06:22 +0900

    messing with photos

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'simple_form'
 gem 'autoprefixer-rails'
 
 # Upload Photo
-gem 'figaro'
+gem 'cloudinary'
 gem 'carrierwave', '~> 0.11.2'
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410092335) do
+ActiveRecord::Schema.define(version: 20170410094249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20170410092335) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "provider_id"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_services_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 20170410092335) do
 
   add_foreign_key "bookings", "services", column: "services_id"
   add_foreign_key "bookings", "users"
+  add_foreign_key "services", "users"
 end
