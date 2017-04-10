31b48ecd4dfb812b5970b484abeaae8a6db41212
Sam Corin <samcorin@gmail.com>
2017-04-10 18:49:33 +0900

    add index to services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,11 @@
 class ServicesController < ApplicationController
+  def index
+    @services = Service.all
+  end
+
+  private
+
+  def set_services
+    @service = Service.find(params[:id])
+  end
 end
