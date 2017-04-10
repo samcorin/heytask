3319a516bfee0284654a9f85c9666237de3d34b4
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-11 01:20:05 +0900

    remove index and show in nested resorces of bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :services, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:show, :index, :new, :create]
+    resources :bookings, only: [:new, :create]
   end
 
   resources :bookings, only: [:index, :show]
