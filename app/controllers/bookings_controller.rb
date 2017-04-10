3e91f9c876272f37c238b3dfcee7d6fb4af5f335
Sam Corin <samcorin@gmail.com>
2017-04-10 18:03:30 +0900

    add user controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,13 @@
+class BookingsController < ApplicationController
+  def title:string
+  end
+
+  def user:references
+  end
+
+  def service:references
+  end
+
+  def status:string
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookings/title:string'
+
+  get 'bookings/user:references'
+
+  get 'bookings/service:references'
+
+  get 'bookings/status:string'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
