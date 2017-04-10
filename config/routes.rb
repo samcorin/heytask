809a3e9f0839c36f42aab900c06a0e30aef17df5
Sam Corin <samcorin@gmail.com>
2017-04-10 18:32:51 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :services, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:show, :index]
+  end
+
+  resources :bookings,
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
