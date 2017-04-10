27c3960660f1d4f23ca5393f93fe66ea2062c29a
Sam Corin <samcorin@gmail.com>
2017-04-10 22:42:01 +0900

    add seeds and faker gem

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'faker'
 
 # Upload Photo
 gem 'cloudinary'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Generate new users
+# 10.times do
+#   User.create(email: "#{Faker::Name.first_name}@yahoo.com", password: "123123")
+# end
+
+# Generate services for each user
+User.all.each do |user|
+  service = Service.new(
+    name: Faker::Educator.course,
+    category: Faker::Company.profession,
+    description: Faker::ChuckNorris.fact,
+    price: 42
+  )
+  service.user = user
+  service.save
+end
