4b77f7b76a379d3d1af4c2e01119b530860320ff
Sam Corin <samcorin@gmail.com>
2017-04-11 09:03:07 +0900

    add fb auth gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410094249) do
+ActiveRecord::Schema.define(version: 20170410155412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 20170410094249) do
   create_table "bookings", force: :cascade do |t|
     t.string   "title"
     t.integer  "user_id"
-    t.integer  "services_id"
+    t.integer  "service_id"
     t.string   "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["services_id"], name: "index_bookings_on_services_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["service_id"], name: "index_bookings_on_service_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20170410094249) do
     t.string   "profile_photo"
     t.text     "bio"
     t.string   "username"
-    t.string   "password"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -62,7 +61,7 @@ ActiveRecord::Schema.define(version: 20170410094249) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "bookings", "services", column: "services_id"
+  add_foreign_key "bookings", "services"
   add_foreign_key "bookings", "users"
   add_foreign_key "services", "users"
 end
