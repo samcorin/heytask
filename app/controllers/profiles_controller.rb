9b6e14cfa5f68d1448af146cd44063e5c470f684
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-11 16:20:18 +0900

    added the show page of profile and reafact the profile photo in the navbar by cecile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,15 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: [ :show, :edit ]
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+end
