07e662472223b8067698595472575c7184fb8ce0
Sam Corin <samcorin@gmail.com>
2017-04-11 16:36:31 +0900

    modified show for categories

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resource :categories, only: [:show]
+  resources :categories, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
