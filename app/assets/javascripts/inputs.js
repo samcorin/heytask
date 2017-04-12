efffa5a6f9bc6ab52b3eea4ebb04a3c2a98e24ac
Sam Corin <samcorin@gmail.com>
2017-04-12 23:39:25 +0900

    messing with the input css

--- app/assets/javascripts/inputs.js
+++ app/assets/javascripts/inputs.js
@@ -5,3 +5,11 @@ $('#input-16').on('focusout', function() {
     $('#input-16').css("background-color", "transparent");
   }
 })
+
+$('#input-35').on('focusout', function() {
+if ($('#input-35')[0].value != '') {
+  $('#input-35').css("transform", "translate3d(100%, 0, 0)");
+  $('.input__label-content--kaede').css("text-align", "right");
+} else {
+}
+})
