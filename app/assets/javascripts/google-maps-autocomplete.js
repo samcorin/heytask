495413fb7e751e8308192bbe5d68393370cc8655
Sam Corin <samcorin@gmail.com>
2017-04-12 21:58:52 +0900

    fixed autocomplete, added auto focus to inputs

--- app/assets/javascripts/google-maps-autocomplete.js
+++ app/assets/javascripts/google-maps-autocomplete.js
@@ -1,61 +1,46 @@
-$(document).ready(function() {
-  var service_address = $('#service_address').get(0);
+var autocomplete;
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    (document.getElementById('service_address')), {types: ['geocode']});
+}
 
-  if (service_address) {
-    var autocomplete = new google.maps.places.Autocomplete(service_address, { types: ['geocode'] });
-    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-    google.maps.event.addDomListener(service_address, 'keydown', function(e) {
-      if (e.keyCode == 13) {
-        e.preventDefault(); // Do not submit the form on Enter.
-      }
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
     });
+  }
+}
+
+$('#service_address').on('focusin', function() {
+  initAutocomplete();
+})
+
+// TODO - form valudation for new service
+
+// disable enter for submit, instead next focus
+$(".form-control").on('keydown', function(e) {
+  if (e.which == 13 && $(this)[0].id == 'service_price' ) {
+    console.log("GOOD")
+  } else if (e.which == 13) {
+    e.preventDefault();
+    var index = $('.form-control').index(document.activeElement) + 1;
+    $('.form-control').eq(index).focus();
   }
 });
 
-function onPlaceChanged() {
-  var place = this.getPlace();
-  var components = getAddressComponents(place);
 
-  $('#service_address').trigger('blur').val(components.address);
-  $('#service_zip_code').val(components.zip_code);
-  $('#service_city').val(components.city);
-  if (components.country_code) {
-    $('#service_country').val(components.country_code);
-  }
-}
 
-function getAddressComponents(place) {
-  // If you want lat/lng, you can look at:
-  // - place.geometry.location.lat()
-  // - place.geometry.location.lng()
-
-  var street_number = null;
-  var route = null;
-  var zip_code = null;
-  var city = null;
-  var country_code = null;
-  for (var i in place.address_components) {
-    var component = place.address_components[i];
-    for (var j in component.types) {
-      var type = component.types[j];
-      if (type == 'street_number') {
-        street_number = component.long_name;
-      } else if (type == 'route') {
-        route = component.long_name;
-      } else if (type == 'postal_code') {
-        zip_code = component.long_name;
-      } else if (type == 'locality') {
-        city = component.long_name;
-      } else if (type == 'country') {
-        country_code = component.short_name;
-      }
-    }
-  }
-
-  return {
-    address: street_number == null ? route : (street_number + ' ' + route),
-    zip_code: zip_code,
-    city: city,
-    country_code: country_code
-  };
-}
+$('#service_address').on('focusout', function() {
+  setTimeout(function() {
+    console.log($('#service_address').val());
+  }, 200);
+})
