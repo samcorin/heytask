6430d2355966686ada6e91ee94529b9f5cd62847
Sam Corin <samcorin@gmail.com>
2017-04-14 11:39:47 +0900

    fixed fb-profile in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
   has_many :gigs, through: :services, source: :bookings
 
   # Geocoding
-  geocoded_by :ip_address,
-    :latitude => :lat, :longitude => :lon
-  after_validation :geocode
+  # geocoded_by :ip_address,
+  #   :latitude => :lat, :longitude => :lon
+  # after_validation :geocode
 
 
 
