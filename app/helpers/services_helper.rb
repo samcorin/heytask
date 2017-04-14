89a20140b0e11fd33b969c1845acd1d8ea632e53
Sam Corin <samcorin@gmail.com>
2017-04-14 12:04:30 +0900

    add default photo to service if not provided

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,9 @@
 module ServicesHelper
+  def picture_url(service)
+    if service.photo.url
+      return service.photo
+    else
+      return 'https://lorempixel.com/400/200/business'
+    end
+  end
 end
