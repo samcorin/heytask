7896b2af855704851906b7d3d5e268b63f89021d
Sam Corin <samcorin@gmail.com>
2017-04-14 15:35:02 +0900

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.destroy_all
 # Generate new users
 puts "Creating users..."
 30.times do
-  User.create!(email: "#{Faker::Name.first_name}@yahoo.com", password: "123123")
+  User.create!(email: "#{Faker::Name.first_name}@yahoo.com", password: "123123", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
 
 # Generate services for each user
